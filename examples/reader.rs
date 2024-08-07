c60e569f8ae0dd40483b7a55fe53b2356ead0a80
Alexey Timin <atimin@gmail.com>
2024-08-07 13:40:30 +0200

    fmt

--- examples/reader.rs
+++ examples/reader.rs
@@ -1,30 +1,20 @@
-use std::pin::pin;
-use reduct_rs::ReductClient;
 use futures_util::StreamExt;
+use reduct_rs::ReductClient;
+use std::pin::pin;
 #[tokio::main]
 async fn main() {
     // Connect to ReductStore instance at 8383 port
     let client = ReductClient::builder().url("http://127.0.0.1:8383").build();
 
     // Get bucket named 'mqtt'
-    let bucket = client
-        .get_bucket("mqtt")
-        .await
-        .unwrap();
+    let bucket = client.get_bucket("mqtt").await.unwrap();
 
     // Read all entries from the bucket
-    let entries = bucket
-        .entries()
-        .await
-        .unwrap();
+    let entries = bucket.entries().await.unwrap();
 
     for entry in entries {
         // Query all records from the entry
-        let mut record_stream = bucket
-            .query(&entry.name)
-            .send()
-            .await
-            .unwrap();
+        let mut record_stream = bucket.query(&entry.name).send().await.unwrap();
 
         // Retrieve records as a stream
         let mut record_stream = pin!(record_stream);
@@ -38,5 +28,4 @@ async fn main() {
             );
         }
     }
-
 }
